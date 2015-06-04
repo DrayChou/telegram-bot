1dc5e91435cae5a9be3496b1f49bab340c14c9bd
dray <hnzksq@gmail.com>
2015-06-04 11:24:55 +0800

    调试代码

--- plugins/service_template.lua
+++ plugins/service_template.lua
@@ -1,4 +1,10 @@
 local function run(msg, matches)
+    dump(msg)
+    
+    dump(chat_info(msg.to.id))
+    
+    send_msg (msg.from.print_name, 'pong', ok_cb, false)
+    
    -- avoid this plugins to process user messages
    if not msg.realservice then
       -- return "Are you trying to troll me?"
