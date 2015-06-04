72a7e398142c2bf97fb3f4af251aa69fbcdc1b74
dray <hnzksq@gmail.com>
2015-06-05 01:27:20 +0800

    整理测试代码

--- plugins/service_template.lua
+++ plugins/service_template.lua
@@ -1,276 +1,486 @@
---[[ DataDumper.lua
-Copyright (c) 2007 Olivetti-Engineering SA
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-]]
-
-local dumplua_closure = [[
-local closures = {}
-local function closure(t) 
-  closures[#closures+1] = t
-  t[1] = assert(loadstring(t[1]))
-  return t[1]
-end
-
-for _,t in pairs(closures) do
-  for i = 2,#t do 
-    debug.setupvalue(t[1], i-1, t[i]) 
-  end 
-end
-]]
-
-local lua_reserved_keywords = {
-    'and', 'break', 'do', 'else', 'elseif', 'end', 'false', 'for', 
-    'function', 'if', 'in', 'local', 'nil', 'not', 'or', 'repeat', 
-'return', 'then', 'true', 'until', 'while' }
-
-local function keys(t)
-    local res = {}
-    local oktypes = { stringstring = true, numbernumber = true }
-    local function cmpfct(a,b)
-        if oktypes[type(a)..type(b)] then
-            return a < b
-        else
-            return type(a) < type(b)
-        end
-    end
-    for k in pairs(t) do
-        res[#res+1] = k
-    end
-    table.sort(res, cmpfct)
-    return res
-end
-
-local c_functions = {}
-    for _,lib in pairs{'_G', 'string', 'table', 'math', 
-    'io', 'os', 'coroutine', 'package', 'debug'} do
-    local t = _G[lib] or {}
-    lib = lib .. "."
-    if lib == "_G." then lib = "" end
-    for k,v in pairs(t) do
-        if type(v) == 'function' and not pcall(string.dump, v) then
-            c_functions[v] = lib..k
-        end
-    end
-end
-
-function DataDumper(value, varname, fastmode, ident)
-    local defined, dumplua = {}
-    -- Local variables for speed optimization
-    local string_format, type, string_dump, string_rep = 
-    string.format, type, string.dump, string.rep
-    local tostring, pairs, table_concat = 
-    tostring, pairs, table.concat
-    local keycache, strvalcache, out, closure_cnt = {}, {}, {}, 0
-        setmetatable(strvalcache, {__index = function(t,value)
-            local res = string_format('%q', value)
-            t[value] = res
-            return res
-    end})
-    local fcts = {
-        string = function(value) return strvalcache[value] end,
-        number = function(value) return value end,
-        boolean = function(value) return tostring(value) end,
-        ['nil'] = function(value) return 'nil' end,
-        ['function'] = function(value) 
-            return string_format("loadstring(%q)", string_dump(value)) 
-        end,
-        userdata = function() error("Cannot dump userdata") end,
-        thread = function() error("Cannot dump threads") end,
-    }
-    local function test_defined(value, path)
-        if defined[value] then
-            if path:match("^getmetatable.*%)$") then
-                out[#out+1] = string_format("s%s, %s)\n", path:sub(2,-2), defined[value])
-            else
-                out[#out+1] = path .. " = " .. defined[value] .. "\n"
-            end
-            return true
-        end
-        defined[value] = path
-    end
-    local function make_key(t, key)
-        local s
-        if type(key) == 'string' and key:match('^[_%a][_%w]*$') then
-            s = key .. "="
-        else
-            s = "[" .. dumplua(key, 0) .. "]="
-        end
-        t[key] = s
-        return s
-    end
-    for _,k in ipairs(lua_reserved_keywords) do
-        keycache[k] = '["'..k..'"] = '
-    end
-    if fastmode then 
-        fcts.table = function (value)
-            -- Table value
-            local numidx = 1
-            out[#out+1] = "{"
-            for key,val in pairs(value) do
-                if key == numidx then
-                    numidx = numidx + 1
-                else
-                    out[#out+1] = keycache[key]
-                end
-                local str = dumplua(val)
-                out[#out+1] = str..","
-            end
-            if string.sub(out[#out], -1) == "," then
-                out[#out] = string.sub(out[#out], 1, -2);
-            end
-            out[#out+1] = "}"
-            return "" 
-        end
-    else 
-        fcts.table = function (value, ident, path)
-            if test_defined(value, path) then return "nil" end
-            -- Table value
-            local sep, str, numidx, totallen = " ", {}, 1, 0
-            local meta, metastr = (debug or getfenv()).getmetatable(value)
-            if meta then
-                ident = ident + 1
-                metastr = dumplua(meta, ident, "getmetatable("..path..")")
-                totallen = totallen + #metastr + 16
-            end
-            for _,key in pairs(keys(value)) do
-                local val = value[key]
-                local s = ""
-                local subpath = path
-                if key == numidx then
-                    subpath = subpath .. "[" .. numidx .. "]"
-                    numidx = numidx + 1
-                else
-                    s = keycache[key]
-                    if not s:match "^%[" then subpath = subpath .. "." end
-                    subpath = subpath .. s:gsub("%s*=%s*$","")
-                end
-                s = s .. dumplua(val, ident+1, subpath)
-                str[#str+1] = s
-                totallen = totallen + #s + 2
-            end
-            if totallen > 80 then
-                sep = "\n" .. string_rep("  ", ident+1)
-            end
-            str = "{"..sep..table_concat(str, ","..sep).." "..sep:sub(1,-3).."}" 
-            if meta then
-                sep = sep:sub(1,-3)
-                return "setmetatable("..sep..str..","..sep..metastr..sep:sub(1,-3)..")"
-            end
-            return str
-        end
-        fcts['function'] = function (value, ident, path)
-            if test_defined(value, path) then return "nil" end
-            if c_functions[value] then
-                return c_functions[value]
-            elseif debug == nil or debug.getupvalue(value, 1) == nil then
-                return string_format("loadstring(%q)", string_dump(value))
-            end
-            closure_cnt = closure_cnt + 1
-            local res = {string.dump(value)}
-            for i = 1,math.huge do
-                local name, v = debug.getupvalue(value,i)
-                if name == nil then break end
-                res[i+1] = v
-            end
-            return "closure " .. dumplua(res, ident, "closures["..closure_cnt.."]")
-        end
-    end
-    function dumplua(value, ident, path)
-        return fcts[type(value)](value, ident, path)
-    end
-    if varname == nil then
-        varname = "return "
-    elseif varname:match("^[%a_][%w_]*$") then
-        varname = varname .. " = "
-    end
-    if fastmode then
-        setmetatable(keycache, {__index = make_key })
-        out[1] = varname
-        table.insert(out,dumplua(value, 0))
-        return table.concat(out)
-    else
-        setmetatable(keycache, {__index = make_key })
-        local items = {}
-        for i=1,10 do items[i] = '' end
-        items[3] = dumplua(value, ident or 0, "t")
-        if closure_cnt > 0 then
-            items[1], items[6] = dumplua_closure:match("(.*\n)\n(.*)")
-            out[#out+1] = ""
-        end
-        if #out > 0 then
-            items[2], items[4] = "local t = ", "\n"
-            items[5] = table.concat(out)
-            items[7] = varname .. "t"
-        else
-            items[2] = varname
-        end
-        return table.concat(items)
-    end
-end
-
--- Define a shortcut function for testing
-function dump(...)
-    print(DataDumper(...), "\n---")
-end
-
 --[[
-return {
-  date=1433394474,
-  flags=16,
-  from={
-    access_hash=1,
-    first_name="Dray",
-    flags=528,
-    id=64163268,
-    last_name="🌞",
-    phone="16128881024",
-    print_name="Dray_🌞",
-    type="user" 
+vardump msg: 
+{
+  date = 1433438016,
+  flags = 16,
+  from = {
+    access_hash = 1,
+    first_name = "Dray",
+    flags = 528,
+    id = 64163268,
+    last_name = "🌞",
+    phone = "16128881024",
+    print_name = "Dray_🌞",
+    type = "user"
   },
-  id="149683",
-  out=false,
-  service=false,
-  text="!!tgservice 123",
-  to={
-    first_name="Bot",
-    flags=144,
-    id=109098660,
-    last_name="DC",
-    phone="13134829895",
-    print_name="Bot_DC",
-    type="user" 
+  id = "157104",
+  out = false,
+  service = false,
+  text = "kick",
+  to = {
+    first_name = "Bot",
+    flags = 144,
+    id = 109098660,
+    last_name = "DC",
+    phone = "13134829895",
+    print_name = "Bot_DC",
+    type = "user"
   },
-  unread=true 
+  unread = true
 }
+vardump chat_info: 
+false
+
+
+vardump msg: 
+{
+  date = 1433438082,
+  flags = 16,
+  from = {
+    access_hash = 1,
+    first_name = "Dray",
+    flags = 528,
+    id = 64163268,
+    last_name = "🌞",
+    phone = "16128881024",
+    print_name = "Dray_🌞",
+    type = "user"
+  },
+  id = "157108",
+  out = false,
+  service = false,
+  text = "kick",
+  to = {
+    flags = 16,
+    id = 17071158,
+    members = {
+      {
+        access_hash = 1,
+        first_name = "Star",
+        flags = 24,
+        id = 63406757,
+        last_name = "Brilliant",
+        print_name = "Star_Brilliant",
+        real_first_name = "Star",
+        real_last_name = "Brilliant",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "qiang",
+        flags = 16,
+        id = 91110543,
+        print_name = "qiang",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Akshay",
+        flags = 16,
+        id = 5750844,
+        print_name = "Akshay",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "enin",
+        flags = 16,
+        id = 103502298,
+        last_name = "An",
+        print_name = "enin_An",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "arch",
+        flags = 16,
+        id = 109485440,
+        last_name = "z",
+        print_name = "arch_z",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Jack",
+        flags = 16,
+        id = 54759533,
+        last_name = "Soo",
+        print_name = "Jack_Soo",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Kyle",
+        flags = 16,
+        id = 53584340,
+        last_name = "Ch",
+        print_name = "Kyle_Ch",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Ｂｏｔｍａｎ",
+        flags = 16,
+        id = 35794149,
+        print_name = "Ｂｏｔｍａｎ",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Sam",
+        flags = 16,
+        id = 101164042,
+        last_name = "Sun",
+        print_name = "Sam_Sun",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = ".",
+        flags = 16,
+        id = 73213962,
+        last_name = "Yip",
+        print_name = "._Yip",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Optimus",
+        flags = 16,
+        id = 74971568,
+        last_name = "Prime",
+        print_name = "Optimus_Prime",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Telegram",
+        flags = 16,
+        id = 33333782,
+        last_name = "Bot",
+        print_name = "Telegram_Bot",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Sweetiebot",
+        flags = 16,
+        id = 50441674,
+        print_name = "Sweetiebot",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "MC",
+        flags = 16,
+        id = 39869021,
+        last_name = "Faguette",
+        print_name = "MC_Faguette",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Dray",
+        flags = 528,
+        id = 64163268,
+        last_name = "🌞",
+        phone = "16128881024",
+        print_name = "Dray_🌞",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Umit",
+        flags = 16,
+        id = 79325645,
+        last_name = "(Toby Cao)",
+        print_name = "Umit_(Toby_Cao)",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "T.",
+        flags = 16,
+        id = 96704724,
+        last_name = "C.",
+        print_name = "T._C.",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Keran",
+        flags = 16,
+        id = 104158516,
+        last_name = "Cho",
+        print_name = "Keran_Cho",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Frank",
+        flags = 16,
+        id = 91025105,
+        print_name = "Frank",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Tony",
+        flags = 16,
+        id = 75508523,
+        last_name = "Wu",
+        print_name = "Tony_Wu",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "malong",
+        flags = 16,
+        id = 57018078,
+        last_name = "lee",
+        print_name = "malong_lee",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "小爱豆",
+        flags = 16,
+        id = 87541146,
+        print_name = "小爱豆",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Signal",
+        flags = 16,
+        id = 102853582,
+        last_name = "Seeds",
+        print_name = "Signal_Seeds",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Noah",
+        flags = 16,
+        id = 100517449,
+        last_name = "Sin",
+        print_name = "Noah_Sin",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "小",
+        flags = 16,
+        id = 110921271,
+        last_name = "屁股",
+        print_name = "小_屁股",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Dx.",
+        flags = 16,
+        id = 98963936,
+        last_name = "Dennx",
+        print_name = "Dx._Dennx",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Yeechan",
+        flags = 16,
+        id = 69640050,
+        last_name = "Lu",
+        print_name = "Yeechan_Lu",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "pt950",
+        flags = 16,
+        id = 109783307,
+        print_name = "pt950",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "milleelf",
+        flags = 16,
+        id = 53854830,
+        print_name = "milleelf",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Radium",
+        flags = 16,
+        id = 61330658,
+        print_name = "Radium",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Li",
+        flags = 16,
+        id = 31013380,
+        print_name = "Li",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Jelly",
+        flags = 16,
+        id = 100270920,
+        print_name = "Jelly",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Chanple",
+        flags = 16,
+        id = 102064612,
+        print_name = "Chanple",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Basith",
+        flags = 16,
+        id = 49858089,
+        print_name = "Basith",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "阿德",
+        flags = 16,
+        id = 97061489,
+        last_name = "阿拉",
+        print_name = "阿德_阿拉",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "sandoo",
+        flags = 16,
+        id = 77677077,
+        last_name = "liang",
+        print_name = "sandoo_liang",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Dr. Merkwürdigliebe",
+        flags = 16,
+        id = 102430753,
+        print_name = "Dr._Merkwürdigliebe",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Bot",
+        flags = 16,
+        id = 103260651,
+        print_name = "Bot",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "小",
+        flags = 16,
+        id = 90580115,
+        last_name = "溪流",
+        print_name = "小_溪流",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Jaconda",
+        flags = 16,
+        id = 72006705,
+        print_name = "Jaconda",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Jesse",
+        flags = 16,
+        id = 77076588,
+        print_name = "Jesse",
+        type = "user"
+      },
+      {
+        first_name = "Bot",
+        flags = 144,
+        id = 109098660,
+        last_name = "DC",
+        phone = "13134829895",
+        print_name = "Bot_DC",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Fwolf",
+        flags = 16,
+        id = 99940312,
+        print_name = "Fwolf",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Kane",
+        flags = 16,
+        id = 69267645,
+        print_name = "Kane",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Liberbot",
+        flags = 16,
+        id = 100547061,
+        print_name = "Liberbot",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "otouto",
+        flags = 16,
+        id = 66603334,
+        print_name = "otouto",
+        type = "user"
+      },
+      {
+        access_hash = 1,
+        first_name = "Kunana",
+        flags = 16,
+        id = 108512323,
+        last_name = "XP",
+        print_name = "Kunana_XP",
+        type = "user"
+      },
+      [0] = {
+        access_hash = 1,
+        first_name = "dabao",
+        flags = 16,
+        id = 96586640,
+        last_name = "ou",
+        print_name = "dabao_ou",
+        type = "user"
+      }
+    },
+    members_num = 48,
+    print_name = "Bots-测试请和机器人私聊，勿在公共频道刷",
+    title = "Bots-测试请和机器人私聊，勿在公共频道刷",
+    type = "chat"
+  },
+  unread = true
+}
+vardump chat_info: 
+false
 ]]
 
 local function run(msg, matches)
-    print("msg: ")
-    dump(msg)
-    
-    print("chat_info: ")
-    dump(chat_info(msg.to.id))
-    
     print("vardump msg: ")
     vardump(msg)
     
