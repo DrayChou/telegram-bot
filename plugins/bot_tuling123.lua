d9a631d6108934b27549d95a1f3b4c3583e45123
dray <hnzksq@gmail.com>
2015-06-11 17:48:47 +0800

    添加图灵机器人

---
+++ plugins/bot_tuling123.lua
@@ -0,0 +1,36 @@
+do
+
+local tuling_config = load_from_file('data/tuling.lua')
+-- 图灵机器人的KEY
+local tuling_url = "http://www.tuling123.com/openapi/api"
+local consumer_key = tuling_config.consumer_key;
+
+local function getTuling(user_id,info)
+    local url = tuling_url.."&key="..consumer_key
+    url=url.."&info="..info
+    url=url.."&userid="..user_id
+    
+    local res,status = http.request(url)
+    
+    if status ~= 200 then return nil end
+    local data = json:decode(res)
+    
+    return data.text
+end
+
+local function run(msg, matches)
+    return getTuling(msg.from.id,matches[1])
+end
+
+return {
+    description = "询问小机器人", 
+    usage = {
+        "bot info: 请求图灵的机器人接口，并返回回答",
+    },
+    patterns = {
+        "^bot (w+)$",
+    }, 
+    run = run 
+}
+
+end
