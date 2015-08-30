e0fa8c2a34acc4b2ad955add69f68c0f5f4856bb
dray <hnzksq@gmail.com>
2015-08-30 16:32:34 +0800

    123

--- plugins/dc.stats.lua
+++ plugins/dc.stats.lua
@@ -87,9 +87,7 @@ local function get_users(msg, day_id)
         
         local users_info = {}
         -- 从用户消息的受众那边拿到用户列表
-        if msg.to.members then
-			vardump(msg.to.members)
-	        
+        if msg.to.members then	        
             for i,user in pairs(msg.to.members) do
                 if user.type == 'user' then
                     local user_id = user.id
