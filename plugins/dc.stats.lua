ee5973979072829936d97d47bff7bde3071a6258
dray <hnzksq@gmail.com>
2015-08-30 17:25:27 +0800

    123

--- plugins/dc.stats.lua
+++ plugins/dc.stats.lua
@@ -7,20 +7,17 @@ local NUM_MSG_MAX = 5
 local TIME_CHECK = 4 -- seconds
 local DEFAULT_SHOW_LIMIT = 10 -- 显示的最多条数
 
-
+local users_info = {}
 -- 回调函数 返回所有成员的信息
 local function returnids(cb_extra, success, result)
-	local user_list = {}
 	local receiver = cb_extra.receiver
 	local chat_id = "chat#id"..result.id
    
 	for k,v in pairs(result.members) do
 	 	local user_id = user.id
 	    local user_info = get_msgs_user_chat(user_id, chat_id, day_id)
-	    table.insert(user_list, user_info)
+	    table.insert(users_info, user_info)
 	end
-	
-	return user_list
 end
 
 local function user_print_name(user)
@@ -105,18 +102,10 @@ local function get_users(msg, day_id)
     	local res = chat_info(chat, returnids, {receiver=receiver})
 		vardump("用户列表：")
 		vardump(res)
+		vardump(users_info)
         
-        local users_info = {}
         -- 从用户消息的受众那边拿到用户列表
-        if res then
-            for i,user in pairs(res) do
-                if user.type == 'user' then
-                    local user_id = user.id
-                    local user_info = get_msgs_user_chat(user_id, chat_id, day_id)
-                    table.insert(users_info, user_info)
-                end
-            end
-        else
+        if table.getn(users_info) == 0 then
 		    local hash = 'chat:'..chat_id..':users'
 		    local users = redis:smembers(hash)
 		    --        -- Get user info
